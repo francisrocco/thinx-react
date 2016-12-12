c40f1d343fb037ae47a70d1362029703fc85221a
Francis Rocco <roccofrancis@gmail.com>
2016-12-12 13:21:45 -0500

    react-create-app on Heroku

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -6,6 +6,7 @@ export default function Welcome(props){
   return (
     <div className="col-md-6 col-md-offset-3">
       <center>
+        <img className="home-img" src="/homepage.jpg" />
         <h3>Lucky you! You made it to a beta version of the site. You don't have to log in just yet!</h3><br/>
         <Link className="btn btn-primary" to={'/posts'}>Click Here to View Posts</Link>
       </center>
